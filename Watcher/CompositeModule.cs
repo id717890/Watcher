53810d7c70a78411255e3f290e3526d5c72ac221
YusupovZA <jusupovz@gmail.com>
2019-06-28 07:57:40 +0300

    init

---
+++ Watcher/CompositeModule.cs
@@ -0,0 +1,23 @@
+﻿using Ninject.Modules;
+using ShutdownDiagnostic.Interface.Model;
+using ShutdownDiagnostic.Interface.Presenter;
+using ShutdownDiagnostic.Interface.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShutdownDiagnostic
+{
+    public class CompositeModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IDiagnosticPresenter>().To<DiagnosticPresenter>();
+            Bind<IDiagnosticView>().To<DiagnosticWindow>();
+            Bind<IDiagnosticViewMinimize>().To<DiagnosticWindowMinimize>();
+            Bind<IDiagnosticViewModel>().To<DiagnosticViewModel>();
+        }
+    }
+}
